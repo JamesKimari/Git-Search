c626483866c7291010eac8117ceef1b8b32fbbd9
JamesKimari <jamesgkimari@gmail.com>
2018-01-15 07:32:05 +0300

    imported HttpClient and HttpHeaders into the gitsearch service

--- src/app/github/gitsearch.service.ts
+++ src/app/github/gitsearch.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class GitsearchService {
 
-  constructor() { }
+  private usernmame: string;
+      
+
+  constructor(private http: HttpClient) { }
 
 }
